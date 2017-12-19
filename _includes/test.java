482dd915239d127935aee5256909c6eb6c749e77
Woosuk Lee <wslee@ropas.snu.ac.kr>
2017-12-18 23:48:20 -0500

    temp

---
+++ _includes/test.java
@@ -0,0 +1,28 @@
+public class test {
+	public static void main(String[] args) {
+		System.out.println("Add 1 + 2:");
+		System.out.println("Result = " + add(1,2));
+		System.out.println("Add 40 + 2:");
+		System.out.println("Result = " + add(40,2));
+		System.out.println("Multiply 3 x 4:");
+		System.out.println("Result = " + multiply(3,4));
+		System.out.println("Multiply 12 x 20:");
+		System.out.println("Result = " + add(12,20));
+		System.out.println("Done!")
+	}
+
+	public static int add(int a, int b) {
+		Integer result = null;
+		result += a;
+		result += b;
+		return result;
+	}
+
+	public static int multiply(int a, int b) {
+		int result = -1;
+		for (int i = 0; i < b; i++)
+			result = add(result, a);
+		return result;
+	}
+}
+
